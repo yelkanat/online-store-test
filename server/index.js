38c488b54832565b44ef6b6f96e091b0b47762d6
yelkanat <elkanat01@gmail.com>
2022-09-01 15:26:55 +0600

    настройка сервера для mock JSON

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const sequelize = require("./db");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
