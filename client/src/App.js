45edce8db72edc5cc61efbe988b41df7e7dcd3d7
yelkanat <elkanat01@gmail.com>
2022-09-01 17:48:54 +0600

    create client app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { useContext, useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  );
+};
+
+export default App;
